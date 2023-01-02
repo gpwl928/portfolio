11c121ffa50fc2adb3a068f0124c0a724488a2e7
gpwl928 <gpwl928@gmail.com>
2023-01-02 18:29:22 +0900

    header컴포넌트 생성

---
+++ pages/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const MainHeader = styled.header`
+  border-bottom: 1px solid gray;
+`;
+
+const Header = () => {
+  return (
+    <MainHeader></MainHeader>
+  );
+};
+
+export default Header;
