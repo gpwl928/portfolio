0521d61c835c4c5a538a1b058e7dbdffb823938d
gpwl928 <gpwl928@gmail.com>
2023-01-03 19:01:31 +0900

    모바일헤더 작업중

--- pages/src/components/Header.tsx
+++ pages/src/components/Header.tsx
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { SerializedStyles } from '@emotion/utils';
 import { useRouter } from 'next/router';
 import { HEADER_ITEM_LIST } from '../../index';
+
+const HamburgerBtnStyle = css`
+  width: 100%;
+  height: 2px;
+  background: gray;
+`;
 
 const MainHeader = styled.header`
   display: flex;
@@ -11,21 +18,28 @@ const MainHeader = styled.header`
   top: 0;
   padding: 0 14px;
   width: 100%;
-  height: 60px;
+  height: ${(props): number => props.theme.HEADER_HEIGHT}px;
   background: rgba(255, 255, 255, 0.1);
   backdrop-filter: saturate(180%) blur(15px);
+  @media (max-width: ${(props): number => props.theme.MOBILE_LANDSCAPE_MAX}px) {
+    padding: 0;
+    height: ${(props): number => props.theme.MOBILE_HEADER_HEIGHT}px;
+  }
 `;
 
-const Logo = styled.div`
-  font-weight: 800;
-  font-size: 2.5rem;
-`;
+// const Logo = styled.div`
+//   font-weight: 800;
+//   font-size: 2.5rem;
+// `;
 
 const MenuUl = styled.ul`
   display: flex;
   height: 100%;
   gap: 16px;
   margin: 0 auto;
+  @media (max-width: ${(props): number => props.theme.MOBILE_LANDSCAPE_MAX}px) {
+    display: none;
+  }
 `;
 
 const MenuLi = styled.li<{ isActvie: boolean }>`
@@ -49,12 +63,73 @@ const MenuLi = styled.li<{ isActvie: boolean }>`
   }
 `;
 
+const MobileButtonBox = styled.button`
+  display: none;
+  @media (max-width: ${(props): number => props.theme.MOBILE_LANDSCAPE_MAX}px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-left: auto;
+    width: ${(props): number => props.theme.MOBILE_HEADER_HEIGHT}px;
+    height: 100%;
+  }
+`;
+
+const MobileButton = styled.div<{ isMobile: boolean }>`
+  position: relative;
+  width: 25px;
+  height: 25px;
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    ${HamburgerBtnStyle}
+  }
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    ${HamburgerBtnStyle}
+  }  
+  ${(props): boolean | SerializedStyles | undefined => 
+    props.isMobile && css `
+      &::before {
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%) rotate(45deg);
+      }
+      &::after {
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%) rotate(-45deg);
+      }
+    `
+  }
+`;
+
+const MobileButtonLine = styled.div<{ isMobile: boolean }>`
+  display: inline-block;
+  position: absolute;
+  top: 50%;
+  left: 0;
+  ${HamburgerBtnStyle}
+  ${(props): boolean | SerializedStyles | undefined => 
+    props.isMobile && css `
+      display: none;
+    `
+  }
+`;
+
 interface HeaderProps {
   currentSection?: string;
 }
 
 const Header: React.FC<HeaderProps> = ({ currentSection }) => {
   const router = useRouter();
+
+  const [isMobileVisible, setIsMobileVisible] = useState(false);
 
   const onScrollMove= (id: string): void => {
     router.push(
@@ -66,7 +141,7 @@ const Header: React.FC<HeaderProps> = ({ currentSection }) => {
 
   return (
     <MainHeader>
-      <Logo>Potofolio</Logo>
+      {/* <Logo>Potofolio</Logo> */}
       <MenuUl>
         {HEADER_ITEM_LIST && HEADER_ITEM_LIST.map((item) => {
           return (
@@ -80,6 +155,11 @@ const Header: React.FC<HeaderProps> = ({ currentSection }) => {
           )
         })}
       </MenuUl>
+      <MobileButtonBox onClick={():void => setIsMobileVisible(!isMobileVisible)}>
+        <MobileButton isMobile={isMobileVisible}>
+          <MobileButtonLine isMobile={isMobileVisible} />
+        </MobileButton>
+      </MobileButtonBox>
     </MainHeader>
   );
 };
