98e5a3158ec5559b13c3be0c0b02e6911f8a8fd8
gpwl928 <gpwl928@gmail.com>
2023-01-03 17:14:04 +0900

    스크롤 시 헤더 css 추가 및 관련 코드 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,72 @@
+import { useState, useEffect } from 'react';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { SerializedStyles } from '@emotion/utils';
 import Header from './src/components/Header';
-
-const Test = styled.div``;
+import About from './src/components/About';
+import Career from './src/components/Career';
+import Project from './src/components/Project';
+import Study from './src/components/Study';
 
 const Main = styled.div`
+  margin-top: 60px;
   background: ${(props): string => props.theme.backgroundColor};
-  width: 100vw;
-  height: 100vh;
-
-  ${Test} {
-    color: pink;
-  }
+  width: 100%;
+  height: 100%;
 `;
 
-const Text = styled.h1`
-  color: white;
-`;
+export const HEADER_ITEM_LIST = [
+  {key: 'about'},
+  {key: 'career'},
+  {key: 'project'},
+  {key: 'study'},
+];
+
+const clamp = (value: number): number => Math.max(0, value);
+
+// 스크롤이 해당 포지션에 포함되있는지 여부
+const isBetween = (value: number, floor: number, ceil: number): boolean =>
+  value >= floor && value <= ceil;
 
 export const Home = () => {
+  const [activeId, setActiveId] = useState('');
+
+  useEffect(() => {
+    const listener = ():void => {
+      const scroll = window.pageYOffset;
+      const position = HEADER_ITEM_LIST.map((item) => {
+        const element = document.getElementById(item.key);
+
+        if (!element) return { id: item.key, top: -1, bottom: -1 };
+        const rect = element.getBoundingClientRect();
+        const top = clamp(rect.top + scroll - rect.height / 4);
+        const bottom = clamp(rect.bottom + scroll - rect.height / 4);
+        return { id: item.key, top, bottom };
+      }).find(({ top, bottom }) => isBetween(scroll, top, bottom));
+
+      const currentId = position?.id || '';
+      setActiveId(currentId);
+    }
+
+    listener();
+
+    window.addEventListener('resize', listener);
+    window.addEventListener('scroll', listener);
+
+    return (): void => {
+      window.removeEventListener('resize', listener);
+      window.removeEventListener('scroll', listener);
+    };
+  });
+
   return (
     <>
+      <Header currentSection={activeId} />
       <Main>
-        <Header></Header>
-        <Text>Hi</Text>
-        <Test>Testttttttt</Test>
+        <About />
+        <Career />
+        <Project />
+        <Study />
       </Main>
     </>
   )
--- pages/src/components/Header.tsx
+++ pages/src/components/Header.tsx
@@ -1,36 +1,82 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { SerializedStyles } from '@emotion/utils';
+import { useRouter } from 'next/router';
+import { HEADER_ITEM_LIST } from '../../index';
 
 const MainHeader = styled.header`
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  border-bottom: 1px solid gray;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  padding: 0 14px;
+  width: 100%;
+  height: 60px;
+  background: rgba(255, 255, 255, 0.1);
+  backdrop-filter: saturate(180%) blur(15px);
 `;
 
 const Logo = styled.div`
   font-weight: 800;
-  font-size: 48px;
+  font-size: 2.5rem;
 `;
 
-const MenuUl = styled.ul``;
+const MenuUl = styled.ul`
+  display: flex;
+  height: 100%;
+  gap: 16px;
+  margin: 0 auto;
+`;
 
-const MenuLi = styled.li``;
+const MenuLi = styled.li<{ isActvie: boolean }>`
+  display: flex;
+  align-items: center;
+  padding: 12px;
+  height: 100%;
+  font-size: 18px;
+  /* font-weight: 600; */
+  text-transform: uppercase;
+  cursor: pointer;
+  @media (hover: hover) and (pointer: fine) {
+    &:hover {
+      color: red;
+    }
+  }
+  ${(props): boolean | undefined => 
+    props.isActive && css `
+      color: red;
+    `
+  }
+`;
 
-const HEADER_ITEM_LIST = [
-  {key: 'about'},
-  {key: 'career'},
-  {key: 'project'},
-  {key: 'study'},
-];
+interface HeaderProps {
+  currentSection?: string;
+}
 
-const Header = () => {
+const Header: React.FC<HeaderProps> = ({ currentSection }) => {
+  const router = useRouter();
+
+  const onScrollMove= (id: string): void => {
+    router.push(
+      {
+        hash: id
+      }
+    );
+  }
+
   return (
     <MainHeader>
       <Logo>Potofolio</Logo>
       <MenuUl>
-        {HEADER_ITEM_LIST.length !== 0 && HEADER_ITEM_LIST.map((item) => {
+        {HEADER_ITEM_LIST && HEADER_ITEM_LIST.map((item) => {
           return (
-            <MenuLi key={item.key}>{item.key}</MenuLi>
+            <MenuLi 
+              key={item.key}
+              isActive={ currentSection === item.key }
+              onClick={():void => onScrollMove(item.key)}
+            >
+              {item.key}
+            </MenuLi>
           )
         })}
       </MenuUl>
