54ae8f2ddd9f7fce21c037fd7bb121c5e33506a3
노혜지 <jj0604@wonderpeople.co.kr>
2023-01-09 23:42:42 +0900

    feat : 푸터 작업 완료
    아이콘 링크별 새창 열기 및 주소 복사 기능 추가

--- pages/src/components/Footer.tsx
+++ pages/src/components/Footer.tsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
-import GitSvg from 'public/images/github-mark-white.svg';
+import GitIcon from 'public/images/github.svg';
+import NotionIcon from 'public/images/notion.svg';
+import MailIcon from 'public/images/mail.svg';
 
 const MainFooter = styled.footer`
   background: black;
@@ -7,14 +9,14 @@ const MainFooter = styled.footer`
 
 const Content = styled.div`
   margin: 0 auto;
-  padding: 20px;
+  padding: 24px;
   max-width: 1460px;
   color: #c6c6c6;
   text-align: center;
 `;
 
 const Rights = styled.div`
-  margin-top: 8px;
+  margin-top: 18px;
   font-size: 14px;
   font-family: ${(props): string => props.theme.defaultFontFamily};
   color: #ececec;
@@ -23,29 +25,46 @@ const Rights = styled.div`
 `;
 
 const Icons = styled.div`
-  border: 1px solid red;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 24px;
 
   svg {
-    fill: red;
+    cursor: pointer;
+    &:hover {
+      fill: ${(props): string => props.theme.mainColor};
+    }
   }
 `;
 
-const Icon = styled.div`
-  width: 40px;
-  height: 40px;
-  &:hover {
-    fill: pink;
-  }
-`;
+const LINKS = [
+  { "key": "github", "url": "https://github.com/gpwl928/portfolio" },
+  { "key": "notion", "url": "https://heyyyyggi.notion.site/Hi-I-m-HyeJi-8ed2f64994224788a4267f084b581ce7" },
+  { "key": "mail", "url": "gpwl928@gmail.com" },
+];
 
 const Footer = () => {
+  const onClickIcon = (icon: 'github' | 'notion' | 'mail') => {
+    let link = LINKS.find(item => item['key'] === icon);
+
+    if (icon === 'mail' && link) {
+      window.navigator.clipboard.writeText(link.url).then(() => {
+        alert("이메일 주소 복사 완료!")
+      });
+    } else {
+      window.open(link?.url,' _blank');
+    }
+  };
+
   return (
     <MainFooter>
       <Content>
         <Icons>
-          <Icon>
-            <GitSvg fill="pink" />
-          </Icon>
+          <GitIcon width="40" height="40" fill="white" onClick={():void => {onClickIcon('github')}}/>
+          <NotionIcon width="40" height="40" fill="white" onClick={():void => {onClickIcon('notion')}}/>
+          <MailIcon width="40" height="40" fill="white" onClick={():void => {onClickIcon('mail')}}/>
         </Icons>
         <Rights>© 2023 — Website created using Next.js / All rights reserved</Rights>
       </Content>
